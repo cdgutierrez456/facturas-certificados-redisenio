86baeeb01b1f7e2cd867c139f9502c85e0024333
Cristian Gutierrez <cdgutierrez456@gmail.com>
2026-02-15 22:56:22 -0500

    Limpiando storage

--- components/modules/payment/PaymentStatus.tsx
+++ components/modules/payment/PaymentStatus.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useEffect } from 'react';
 import { CheckCircle, ClockArrowUp, Ban } from 'lucide-react';
 
 import { usePsePaymentForm } from './hooks/usePsePaymentForm';
@@ -14,6 +15,10 @@ interface PaymentStatusProps {
 
 export default function PaymentStatus({ infoTransaction }: PaymentStatusProps) {
   const { banks } = usePsePaymentForm({})
+
+  useEffect(() => {
+    return () => { localStorage.clear() };
+  }, []);
 
   const getNameBank = (idBank: any) => {
     const selectedBank = banks.find(item => item.financialInstitutionCode == idBank)
